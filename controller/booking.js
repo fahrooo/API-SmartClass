542a9e19d63112736ed076bbd7dbf726b977b38e
fahrooo <fahrooo2808@student.uns.ac.id>
2023-03-29 10:28:30 +0700

    up

--- controller/booking.js
+++ controller/booking.js
@@ -387,12 +387,79 @@ export const deleteBooking = async (req, res) => {
   }
 };
 
+export const scheduleKelas = async (req, res) => {
+  const { id_kelas, waktu_pemesanan } = req.body;
+
+  try {
+    const booking = await db.query(
+      `SELECT
+      * 
+    FROM
+      (
+      SELECT
+        x.ID,
+        x.nama,
+        "count" ( y.id_kelas ) AS jumlah_booking 
+      FROM
+        "classrooms" x
+        LEFT JOIN booking y ON x."id" = y.id_kelas 
+      WHERE
+        x.id_unit = ${id_kelas} 
+        AND DATE ( y.waktu_pemesanan ) = '${waktu_pemesanan}' 
+        AND y.status = 'approve' 
+      GROUP BY
+        x.nama,
+        x.ID 
+      ORDER BY
+        x.nama 
+      ) AS booking_true UNION ALL
+    SELECT ID
+      ,
+      nama,
+      0 AS jumlah_booking 
+    FROM
+      classrooms 
+    WHERE
+      id_unit = ${id_kelas} 
+      AND nama NOT IN (
+      SELECT
+        x.nama 
+      FROM
+        "classrooms" x
+        LEFT JOIN booking y ON x."id" = y.id_kelas 
+      WHERE
+        x.id_unit = ${id_kelas} 
+        AND DATE ( y.waktu_pemesanan ) = '${waktu_pemesanan}' 
+        AND y.status = 'approve' 
+      GROUP BY
+        x.nama,
+        x.ID 
+      ORDER BY
+      x.nama 
+      )`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+
+    return res.status(200).json({
+      status: 200,
+      data: booking,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: error,
+    });
+  }
+};
+
 export const scheduleBooking = async (req, res) => {
   const { id_kelas, waktu_pemesanan } = req.body;
 
   try {
     const booking = await db.query(
-      `SELECT * FROM (SELECT waktu.id, to_char(time_start,'HH24:MI') as time_start, to_char(time_end,'HH24:MI') as time_end, booking.id as id_booking, id_user, id_kelas, id_waktu, waktu_pemesanan, is_booking, status, keterangan, booking."createdAt" FROM "waktu" JOIN booking ON booking.id_waktu=waktu."id" WHERE id_kelas = ${id_kelas} AND date(waktu_pemesanan) = '${waktu_pemesanan}'
+      `SELECT * FROM (SELECT waktu.id, to_char(time_start,'HH24:MI') as time_start, to_char(time_end,'HH24:MI') as time_end, booking.id as id_booking, id_user, id_kelas, id_waktu, waktu_pemesanan, is_booking, status, keterangan, booking."createdAt" FROM "waktu" JOIN booking ON booking.id_waktu=waktu."id" WHERE id_kelas = ${id_kelas} AND date(waktu_pemesanan) = '${waktu_pemesanan}' AND status='approve'
       UNION ALL
       SELECT waktu.id, to_char(time_start,'HH24:MI') as time_start, to_char(time_end,'HH24:MI') as time_end, booking.id as id_booking, id_user, id_kelas, id_waktu, waktu_pemesanan, is_booking, status, keterangan, booking."createdAt" FROM "waktu" LEFT JOIN booking ON booking.id_waktu=NULL WHERE waktu.id NOT IN (SELECT waktu.id FROM "waktu" JOIN booking ON booking.id_waktu=waktu."id" WHERE id_kelas = ${id_kelas} AND date(waktu_pemesanan) = '${waktu_pemesanan}')) AS waktu_booking ORDER BY id ASC`,
       {
